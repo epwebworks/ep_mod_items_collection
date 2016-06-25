4aa250dd945a2a007886f9857625879fdc0acfbb
e.pol <eugene.poletski@gmail.com>
2016-06-25 23:37:12 +0300

    Initial commit

---
+++ js/modules/ep-mod-items-collection/main.js
@@ -0,0 +1,56 @@
+/**
+ * Created by 123 on 25.06.2016.
+ * main.js
+ * Main module script
+ */
+
+/*jslint
+ browser : true,   continue : true,    devel : true,     indent   : 2,
+ maxerr  : 50,     newcap   : true,    nomen : true,     plusplus : true,
+ regexp  : true,   sloppy   : true,    vars : false,     white    : true
+ */
+
+/*global
+ require
+ */
+
+require.config({
+  baseUrl : 'js',
+  paths   : {
+    jquery        : 'libs/jquery/jquery-1.12.3.min',
+    bootstrap     : 'libs/bootstrap/bootstrap.min',
+    underscore    : 'libs/underscore/underscore-min',
+    backbone      : 'libs/backbone/backbone-min',
+    text          : 'libs/require/text',
+    json          : 'libs/require/json',
+    ep_mod_ic     : 'modules/ep-mod-items-collection'
+  },
+  shim    : {
+    'backbone'     : {
+      deps    : ['jquery', 'underscore'],
+      exports : 'Backbone'
+    },
+    'underscore'   : {
+      exports : '_'
+    },
+    'bootstrap'    : {
+      deps    : ['jquery'],
+      exports : 'bootstrap'
+    },
+    'json'         : {
+      deps : ['text']
+    }
+  }
+});
+
+require([
+  'bootstrap',
+  'ep_mod_ic/views/main.view',
+  'json!ep_mod_ic/config.json'
+], function ( bootstrap, MainView, config ) {
+
+  "use strict";
+
+  new MainView({ el : config.ui.main });
+
+});
